2827eef8c3be5159961ebbd96649acea6aa604c1
chuanhai.wei <chuanhai.wei@56qq.com>
2019-04-29 18:54:09 +0800

    http-proxy

--- http-proxy/src/main/java/com/code/ting/netty/proxy/http/chain/context/Status.java
+++ http-proxy/src/main/java/com/code/ting/netty/proxy/http/chain/context/Status.java
@@ -4,5 +4,4 @@ package com.code.ting.netty.proxy.http.chain.context;
 public enum Status {
     NEW,
     CANCEL,
-
 }
--- http-proxy/src/main/java/com/code/ting/netty/proxy/http/chain/processor/AuthProcessor.java
+++ http-proxy/src/main/java/com/code/ting/netty/proxy/http/chain/processor/AuthProcessor.java
@@ -4,6 +4,7 @@ package com.code.ting.netty.proxy.http.chain.processor;
 import com.code.ting.netty.proxy.http.chain.Processor;
 import com.code.ting.netty.proxy.http.chain.YieldResult;
 import com.code.ting.netty.proxy.http.chain.context.Context;
+import com.code.ting.netty.proxy.http.chain.context.Result;
 import org.apache.commons.lang3.StringUtils;
 
 public class AuthProcessor implements Processor {
@@ -16,6 +17,7 @@ public class AuthProcessor implements Processor {
     @Override
     public YieldResult process(Context context) {
         if (StringUtils.isBlank(context.getRequest().getHeader("auth"))) {
+            context.setResult(Result.of("01", "auth error"));
             return YieldResult.FAIL;
         }
 
